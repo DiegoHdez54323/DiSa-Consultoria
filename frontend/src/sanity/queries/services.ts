65442b5c586157b6ab1cccbf15a4d3155bbd4826
DiegoHdez54323 <85593229+DiegoHdez54323@users.noreply.github.com>
2026-01-07 01:49:07 -0600

    servicesFull.ts

--- frontend/src/sanity/queries/services.ts
+++ frontend/src/sanity/queries/services.ts
@@ -1,7 +1,8 @@
 import { defineQuery } from "groq";
+import groq from "groq";
 
-export const SERVICES_QUERY = defineQuery(`
-  *[_type == "servicesType"] | order(_createdAt asc){
+export const SERVICES_QUERY = groq`
+*[_type == "servicesType"] | order(_createdAt asc){
     "id": slug.current,
     title,
     subtitle,
@@ -15,5 +16,4 @@ export const SERVICES_QUERY = defineQuery(`
       tag,
       features
     }
-  }
-`);
+  }`;
